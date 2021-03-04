557cb461ea63ad398399ef8a530f5eb8ef1d960a
Maryam Chaudhry <maryamc1798@gmail.com>
2021-03-03 19:04:07 -0500

    fixing routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom"
 import Main from "./components/Main";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
@@ -7,11 +8,17 @@ import "./App.css";
 
 function App() {
   return (
+    <div>
+    <Router basename={process.env.PUBLIC_URL}>
     <div className="App">
+      
       <Wrapper>
         <Header />
-        <Main />
+        <Route exact path="/" component={Main} />
       </Wrapper>
+      
+    </div>
+    </Router>
     </div>
   );
 }
